e276ed473747149a2c2224ae4cd1c0e66b4b4910
Raymond Akornor <raymondakornor@gmail.com>
2019-03-04 00:33:20 +0000

    move login_required into decorators

---
+++ main/decorators.py
@@ -0,0 +1,19 @@
+import os
+from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
+
+TOKEN = os.path.join(settings.BASE_DIR, 'token.json')
+
+def login_required(function=None, redirect_field_name=None, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, redirecting
+    to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: os.path.exists(TOKEN),
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
--- main/views.py
+++ main/views.py
@@ -1,14 +1,11 @@
 import json
-import os
 from django.shortcuts import render, redirect
-from django.conf import settings
-from django.contrib.auth.decorators import user_passes_test
 from django.http import JsonResponse
-from .tasks import generate_playlist
 from django.views.decorators.http import require_POST
+from .tasks import generate_playlist
+from .decorators import login_required
 from main import oauth
 
-TOKEN = os.path.join(settings.BASE_DIR, 'token.json')
 
 def login(request):
     authorize_url = oauth.get_authorize_url()
@@ -28,21 +25,6 @@ def playlist(request):
         result = generate_playlist.delay(playlist)
         return JsonResponse({'task_id': result.task_id})
     return JsonResponse({'message': "playlist required in payload"})
-
-def login_required(function=None, redirect_field_name=None, login_url=None):
-    """
-    Decorator for views that checks that the user is logged in, redirecting
-    to the log-in page if necessary.
-    """
-    actual_decorator = user_passes_test(
-        lambda u: os.path.exists(TOKEN),
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
-    )
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
-
 
 @login_required(login_url='/login')
 def index(request):
