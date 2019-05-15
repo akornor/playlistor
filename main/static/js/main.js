153d8d8ca0b1b2b3de4454b01d02a0fab780cc5f
Raymond Akornor <raymondakornor@gmail.com>
2019-05-15 19:19:40 +0000

    use async await.

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -1,6 +1,6 @@
 const $ = (window.$ = document.querySelector.bind(document));
 
-const button = $("#btn");
+let button = $("#btn");
 
 const resetProgressBar = () => {
   $("#progress-bar").style.width = "0%";
@@ -9,12 +9,11 @@ const resetProgressBar = () => {
 // this is probably hacky
 //should figure out a better way to reset button
 const resetButton = () => {
-  let button = $("#btn");
   button.innerHTML = "Submit 👍🏽";
   button.disabled = false;
 };
 
-button.onclick = event => {
+button.onclick = async function(event) {
   event.preventDefault();
   const playlist = $("#playlist").value;
   if (!is_valid_url(playlist)) {
@@ -28,29 +27,27 @@ button.onclick = event => {
   resetProgressBar();
   button.innerHTML = "<i class='fa fa-spinner fa-spin '></i>";
   button.disabled = true;
-  fetch("/playlist", {
-    method: "POST",
-    body: JSON.stringify({
-      playlist: playlist
-    }),
-    headers: {
-      "Content-Type": "application/json"
-    }
-  })
-    .then(response => {
-      response.json().then(data => {
-        if ('task_id' in data) {
-          const progressUrl = `/celery-progress/${data.task_id}/`;
-          // console.log(progressUrl);
-          CeleryProgressBar.initProgressBar(progressUrl);
-        } else {
-          CeleryProgressBar.onErrorDefault();
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
+  try {
+    let response = await fetch("/playlist", {
+      method: "POST",
+      body: JSON.stringify({
+        playlist: playlist
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
     });
+    let data = await response.json();
+    if ("task_id" in data) {
+      const progressUrl = `/celery-progress/${data.task_id}/`;
+      CeleryProgressBar.initProgressBar(progressUrl);
+    } else {
+      CeleryProgressBar.onErrorDefault();
+    }
+  } catch (error) {
+    console.log(error);
+    CeleryProgressBar.onErrorDefault();
+  }
 };
 const is_valid_url = str => {
   const regexp = /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
