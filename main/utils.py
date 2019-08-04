678de7abdb8d54a9474de1d29012558703d4157e
Raymond Akornor <raymondakornor@gmail.com>
2019-08-04 02:04:57 +0000

    add redis client.

--- main/utils.py
+++ main/utils.py
@@ -1,4 +1,6 @@
 import requests
+from django.conf import settings
+import redis
 
 def fetch_url(url):
     response = requests.get(url)
@@ -7,3 +9,8 @@ def fetch_url(url):
 
 def grouper(n, iterable):
     return [iterable[i:i + n] for i in range(0, len(iterable), n)]
+
+def get_redis_client():
+	return redis.Redis.from_url(settings.REDIS_URL)
+
+redis_client = get_redis_client()
