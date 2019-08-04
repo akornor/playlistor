1cfe4775d486c56e4fde1d8118429b02042b0b10
Raymond Akornor <raymondakornor@gmail.com>
2019-08-04 02:06:28 +0000

    Refactor main.js.

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -9,14 +9,17 @@ const resetProgressBar = () => {
 // this is probably hacky
 //should figure out a better way to reset button
 const resetButton = () => {
-  button.innerHTML = "Submit 👍🏽";
+  button.innerHTML = "Convert";
   button.disabled = false;
 };
 
 button.onclick = async function(event) {
   event.preventDefault();
   const playlist = $("#playlist").value;
-  if (!is_valid_url(playlist)) {
+  if (playlist === "") {
+    return;
+  }
+  if (!is_valid_url(playlist.trim())) {
     swal(
       "Invalid URL",
       "Enter valid url e.g https://itunes.apple.com/us/playlist/ep-3-paak-house-radio-playlist/pl.be45d23328f642cc91cf7086c7126daf"
@@ -28,28 +31,26 @@ button.onclick = async function(event) {
   button.innerHTML = "<i class='fa fa-spinner fa-spin '></i>";
   button.disabled = true;
   try {
-    let response = await fetch("/playlist", {
+    const response = await fetch("/playlist", {
       method: "POST",
       body: JSON.stringify({
-        playlist: playlist
+        playlist
       }),
       headers: {
         "Content-Type": "application/json"
       }
     });
-    let data = await response.json();
-    if ("task_id" in data) {
-      const progressUrl = `/celery-progress/${data.task_id}/`;
-      CeleryProgressBar.initProgressBar(progressUrl);
-    } else {
-      CeleryProgressBar.onErrorDefault();
+    if (!response.ok) {
+      throw Error(response.statusText);
     }
+    let { task_id } = await response.json();
+    const progressUrl = `/celery-progress/${task_id}/`;
+    CeleryProgressBar.initProgressBar(progressUrl);
   } catch (error) {
-    console.log(error);
     CeleryProgressBar.onErrorDefault();
   }
 };
 const is_valid_url = str => {
   const regexp = /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-  return regexp.test(str.trim());
+  return regexp.test(str);
 };
