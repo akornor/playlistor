25eb2265cf8fe20f01b8023593ecf499f9e2a219
Raymond Akornor <raymondakornor@gmail.com>
2019-08-04 09:24:17 +0000

    format code with black.

--- main/parsers.py
+++ main/parsers.py
@@ -1,11 +1,13 @@
 from collections import namedtuple
 import re
 
-Track = namedtuple('Track', ['title', 'artist', 'featuring'])
+Track = namedtuple("Track", ["title", "artist", "featuring"])
+
 
 class BaseParser:
     def __init__(self, html_source: str) -> None:
         from bs4 import BeautifulSoup
+
         self._soup = BeautifulSoup(html_source, "html.parser")
 
     def extract_data(self):
@@ -17,31 +19,41 @@ class AppleMusicParser(BaseParser):
         return {
             "playlist_title": self._get_playlist_title(),
             "tracks": self._get_playlist_tracks(),
-            "playlist_creator": self._get_playlist_creator()
+            "playlist_creator": self._get_playlist_creator(),
         }
 
     def _get_playlist_title(self):
-        title = self._soup.find(class_='product-header__title').get_text().strip()
+        title = self._soup.find(class_="product-header__title").get_text().strip()
         return title
 
     def _get_playlist_tracks(self):
         soup = self._soup
         tracks = []
-        tracklist = soup.find_all(class_='tracklist-item--song')
+        tracklist = soup.find_all(class_="tracklist-item--song")
         for track in tracklist:
-            title = track.find(class_='tracklist-item__text__headline').get_text().strip()
-            artist = track.find(class_='table__row__link table__row__link--secondary').get_text().strip().replace('&', ',')
-            featuring = ''
-            if 'feat.' in title:
-                title = title.replace('feat. ', '')
-                mo = re.search(r'\((.*?)\)', title)
+            title = (
+                track.find(class_="tracklist-item__text__headline").get_text().strip()
+            )
+            artist = (
+                track.find(class_="table__row__link table__row__link--secondary")
+                .get_text()
+                .strip()
+                .replace("&", ",")
+            )
+            featuring = ""
+            if "feat." in title:
+                title = title.replace("feat. ", "")
+                mo = re.search(r"\((.*?)\)", title)
                 if mo:
-                    featuring = mo.group(1).replace('&', ',')
-                i = title.find('(')
+                    featuring = mo.group(1).replace("&", ",")
+                i = title.find("(")
                 title = title[:i]
             tracks.append(Track(title=title, artist=artist, featuring=featuring))
         return tracks
 
     def _get_playlist_creator(self):
-        creator = self._soup.find(class_='product-header__identity album-header__identity').get_text().strip()
-        return creator
+        return (
+            self._soup.find(class_="product-header__identity album-header__identity")
+            .get_text()
+            .strip()
+        )
--- main/tasks.py
+++ main/tasks.py
@@ -7,38 +7,47 @@ from .utils import fetch_url, grouper, redis_client
 from main import oauth
 from celery_progress.backend import ProgressRecorder
 
+
 def get_access_token():
-    return oauth.get_cached_token()['access_token']
+    return oauth.get_cached_token()["access_token"]
+
 
 def get_spotify_client(token):
     return Spotify(auth=token)
+
 
 @shared_task(bind=True)
 def generate_playlist(self, url):
     progress_recorder = ProgressRecorder(self)
     token = get_access_token()
     sp = get_spotify_client(token)
-    uid = sp.current_user()['id']
+    uid = sp.current_user()["id"]
     html = fetch_url(url)
     data = AppleMusicParser(html).extract_data()
-    playlist_title = data['playlist_title']
-    tracks = data['tracks']
-    creator = data['playlist_creator']
+    playlist_title = data["playlist_title"]
+    tracks = data["tracks"]
+    creator = data["playlist_creator"]
     n = len(tracks)
-    playlist = sp.user_playlist_create(uid, playlist_title, description=f'Originally created by {creator} on Apple Music[{url}].')
-    playlist_id = playlist['id']
+    playlist = sp.user_playlist_create(
+        uid,
+        playlist_title,
+        description=f"Originally created by {creator} on Apple Music[{url}].",
+    )
+    playlist_id = playlist["id"]
     tracks_uris = []
     try:
         for i, track in enumerate(tracks):
             try:
-                results = sp.search(f'{track.title} {track.artist} {track.featuring}', limit=1)
-                track_uri = results['tracks']['items'][0]["uri"]
+                results = sp.search(
+                    f"{track.title} {track.artist} {track.featuring}", limit=1
+                )
+                track_uri = results["tracks"]["items"][0]["uri"]
                 tracks_uris.append(track_uri)
             except (IndexError, KeyError):
                 continue
             finally:
-                progress_recorder.set_progress(i+1, n)
-        #You can add a maximum of 100 tracks per request.
+                progress_recorder.set_progress(i + 1, n)
+        # You can add a maximum of 100 tracks per request.
         if len(tracks_uris) > 100:
             for chunk in grouper(100, tracks_uris):
                 sp.user_playlist_add_tracks(uid, playlist_id, chunk)
@@ -48,5 +57,14 @@ def generate_playlist(self, url):
         # Delete playlist if error occurs while adding songs
         sp.user_playlist_unfollow(uid, playlist_id)
         raise e
-    redis_client.lpush("playlists", json.dumps({"spotify_url": playlist['external_urls']['spotify'], "applemusic_url": url, "name": playlist_title}))
-    return playlist['external_urls']['spotify']
+    redis_client.lpush(
+        "playlists",
+        json.dumps(
+            {
+                "spotify_url": playlist["external_urls"]["spotify"],
+                "applemusic_url": url,
+                "name": playlist_title,
+            }
+        ),
+    )
+    return playlist["external_urls"]["spotify"]
