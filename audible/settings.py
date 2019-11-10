5fed29e687822bd891f1d64ffa550523219927ca
Raymond Akornor <raymondakornor@gmail.com>
2019-11-10 18:34:21 +0000

    Add template context processor.

--- audible/settings.py
+++ audible/settings.py
@@ -81,6 +81,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'main.context_processors.default_context'
             ],
         },
     },
