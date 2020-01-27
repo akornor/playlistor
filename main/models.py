653b4ec5fefcde65c878de95220bb5e447f1bfb4
Raymond Akornor <raymondakornor@gmail.com>
2020-01-27 14:34:35 +0000

    Add created_at field to playlist.

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ class Playlist(models.Model):
     name = models.CharField(max_length=200)
     spotify_url = models.URLField()
     applemusic_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Playlist({self.name})"
