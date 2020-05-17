eb6e49c847b58d569fae15317b6bb9d5912c11a4
Raymond Akornor <raymondakornor@gmail.com>
2020-05-17 11:46:41 +0000

    Remove whitenoise middleware.

--- playlistor/base_settings.py
+++ playlistor/base_settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
