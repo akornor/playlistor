0c6717a7671d043a74a2572ce8815e08c2a7e66a
Raymond Akornor <raymondakornor@gmail.com>
2020-09-17 16:15:19 +0000

    Refactor code to properly handle command error.

--- manage.py
+++ manage.py
@@ -4,15 +4,13 @@ import sys
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "playlistor.dev_settings")
+    from django.core.management import execute_from_command_line
+    from django.core.management.base import CommandError
     try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+        execute_from_command_line(sys.argv)
+    except CommandError as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == "__main__":
    main() 
