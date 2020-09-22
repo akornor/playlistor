613181810a38c90e07687ddc78b2fc0f70eb6b4c
Raymond Akornor <raymondakornor@gmail.com>
2020-09-22 16:52:33 +0000

    Also retry post requests.

--- main/utils.py
+++ main/utils.py
@@ -21,6 +21,7 @@ def requests_retry_session(
         connect=retries,
         backoff_factor=backoff_factor,
         status_forcelist=status_forcelist,
+        method_whitelist=["HEAD", "GET", "PUT", "DELETE", "OPTIONS", "TRACE", "POST"]
     )
     adapter = HTTPAdapter(max_retries=retry)
     session.mount("http://", adapter)
