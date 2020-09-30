73e9c86cd3bc88eb65cdbe192a05ad9b39fe5946
Raymond Akornor <raymondakornor@gmail.com>
2020-09-30 23:54:30 +0000

    Add keyword arguments.

--- main/__init__.py
+++ main/__init__.py
@@ -2,7 +2,7 @@ from spotipy.oauth2 import SpotifyOAuth
 from django.conf import settings
 
 oauth = SpotifyOAuth(
-    settings.SPOTIFY_CLIENT_ID, settings.SPOTIFY_CLIENT_SECRET, settings.SPOTIFY_REDIRECT_URI,
+    client_id=settings.SPOTIFY_CLIENT_ID, client_secret=settings.SPOTIFY_CLIENT_SECRET, redirect_uri=settings.SPOTIFY_REDIRECT_URI,
     scope='playlist-modify-public playlist-modify-private user-read-private',
     cache_path='token.json'
     )
