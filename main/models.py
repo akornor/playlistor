e63df09f85a8205a19f73e2cfa2a73fc849515c3
Raymond Akornor <raymondakornor@gmail.com>
2020-10-22 17:13:22 +0000

    migrations: Remove featuring field from track.

--- main/models.py
+++ main/models.py
@@ -13,8 +13,7 @@ class Playlist(models.Model):
 class Track(models.Model):
 	MAX_LENGTH = 255
 	name = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
-	artist = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
-	featuring = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
+	artists = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
 	spotify_id = models.CharField(max_length=MAX_LENGTH, null=True, unique=True)
 	apple_music_id = models.CharField(max_length=MAX_LENGTH, null=True, unique=True)
 	isrc = models.CharField(max_length=MAX_LENGTH, null=True)
