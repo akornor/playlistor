43ab3720cbd60bb699480c460dbf22e08655f406
Raymond Akornor <raymondakornor@gmail.com>
2020-12-02 01:50:38 +0000

    Add celery-progress to installed apps.

--- playlistor/base_settings.py
+++ playlistor/base_settings.py
@@ -4,22 +4,24 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 def get_secret(name, default=None):
     return os.environ.get(name, default)
+
 
 def get_from_file_if_exists(path: str) -> str:
     if os.path.exists(path):
         with open(path, "r") as f:
             return f.read()
     else:
-        return ''
+        return ""
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-h7x5-7v-f*=v9+8swk8)cwu^%b(h2btl0nb&@g$es-28ls5pt'
+SECRET_KEY = "-h7x5-7v-f*=v9+8swk8)cwu^%b(h2btl0nb&@g$es-28ls5pt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -27,52 +29,53 @@ SECRET_KEY = '-h7x5-7v-f*=v9+8swk8)cwu^%b(h2btl0nb&@g$es-28ls5pt'
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'main.apps.MainConfig',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "main.apps.MainConfig",
+    "celery_progress",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'playlistor.urls'
+ROOT_URLCONF = "playlistor.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'main/templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'main.context_processors.default_context'
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "main/templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "main.context_processors.default_context",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'playlistor.wsgi.application'
+WSGI_APPLICATION = "playlistor.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -82,26 +85,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -113,26 +110,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-REDIS_URL = get_secret('REDIS_URL')
-SPOTIFY_REDIRECT_URI = get_secret('SPOTIFY_REDIRECT_URI')
-SPOTIFY_CLIENT_ID = get_secret('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = get_secret('SPOTIFY_CLIENT_SECRET')
-APPLE_KEY_ID = get_secret('APPLE_KEY_ID')
-APPLE_TEAM_ID = get_secret('APPLE_TEAM_ID')
-APPLE_PRIVATE_KEY = get_from_file_if_exists(os.path.join(BASE_DIR, 'private.pem'))
+REDIS_URL = get_secret("REDIS_URL")
+SPOTIFY_REDIRECT_URI = get_secret("SPOTIFY_REDIRECT_URI")
+SPOTIFY_CLIENT_ID = get_secret("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = get_secret("SPOTIFY_CLIENT_SECRET")
+APPLE_KEY_ID = get_secret("APPLE_KEY_ID")
+APPLE_TEAM_ID = get_secret("APPLE_TEAM_ID")
+APPLE_PRIVATE_KEY = get_from_file_if_exists(os.path.join(BASE_DIR, "private.pem"))
 
 CACHES = {
-    'default': {
-        'BACKEND': 'redis_cache.RedisCache',
-        'LOCATION': REDIS_URL,
-        'OPTIONS': {
-            'PICKLE_VERSION': 2,
-            'CONNECTION_POOL_CLASS': 'redis.ConnectionPool',
-            'CONNECTION_POOL_CLASS_KWARGS': {
-                'max_connections': 1,
-            }
+    "default": {
+        "BACKEND": "redis_cache.RedisCache",
+        "LOCATION": REDIS_URL,
+        "OPTIONS": {
+            "PICKLE_VERSION": 2,
+            "CONNECTION_POOL_CLASS": "redis.ConnectionPool",
+            "CONNECTION_POOL_CLASS_KWARGS": {"max_connections": 1},
         },
-    },
+    }
 }
