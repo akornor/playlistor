b75cc5142f309bb9536152a63c5a93c9cf685010
Raymond Akornor <raymondakornor@gmail.com>
2021-02-19 16:01:06 +0000

    Modify spotify_url and applemusic_url to be nullable.

--- main/models.py
+++ main/models.py
@@ -4,27 +4,31 @@ from bulk_update_or_create import BulkUpdateOrCreateQuerySet
 
 class Playlist(models.Model):
     name = models.CharField(max_length=200)
-    spotify_url = models.URLField()
-    applemusic_url = models.URLField()
+    spotify_url = models.URLField(null=True)
+    applemusic_url = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Playlist({self.name})"
 
+
 class Track(models.Model):
-	MAX_LENGTH = 255
-	name = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
-	artists = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
-	spotify_id = models.CharField(max_length=MAX_LENGTH, null=True, unique=True)
-	apple_music_id = models.CharField(max_length=MAX_LENGTH, null=True, unique=True)
-	isrc = models.CharField(max_length=MAX_LENGTH, null=True)
+    MAX_LENGTH = 255
+    name = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
+    artists = models.CharField(max_length=MAX_LENGTH, null=True, db_index=True)
+    spotify_id = models.CharField(max_length=MAX_LENGTH, null=True, unique=True)
+    apple_music_id = models.CharField(max_length=MAX_LENGTH, null=True, unique=True)
+    isrc = models.CharField(max_length=MAX_LENGTH, null=True)
 
-	objects = BulkUpdateOrCreateQuerySet.as_manager()
+    objects = BulkUpdateOrCreateQuerySet.as_manager()
 
-	class Meta:
-		constraints = [
-			models.UniqueConstraint(fields=['spotify_id', 'apple_music_id'], name='unique_spotify_id_apple_music_id')
-		]
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["spotify_id", "apple_music_id"],
+                name="unique_spotify_id_apple_music_id",
+            )
+        ]
 
-	def __str__(self):
-		return f"Track({self.name})"
+    def __str__(self):
+        return f"Track({self.name})"
