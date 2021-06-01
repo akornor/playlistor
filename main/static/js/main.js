74998c5aed4c74c2144827c611d207bd04689f7c
Raymond Akornor <raymondakornor@gmail.com>
2021-06-01 22:45:04 +0000

    Improve playlist url validation logic.

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -107,9 +107,8 @@ function onTaskError(progressBarElement, progressBarMessageElement, excMessage) 
         resetButton();
     }
 
-const SPOTIFY_PLAYLIST_URL_REGEX = /open\.spotify\.com\/(user\/.+\/)?playlist\/.+/;
-const APPLE_MUSIC_PLAYLIST_URL_REGEX = /(embed.)?music\.apple\.com\/.+\/playlist\/.+/;
-const SUPPORTED_PLATFORMS_PLAYLIST_URL_REGEX = /open\.spotify\.com\/(user\/.+\/)?playlist\/.+|(embed.)?music\.apple\.com\/.+\/playlist\/.+/;
+const SPOTIFY_PLAYLIST_URL_REGEX = /^http(s):\/\/open\.spotify\.com\/(user\/.+\/)?playlist\/.+$/;
+const APPLE_MUSIC_PLAYLIST_URL_REGEX = /^http(s):\/\/(embed.)?music\.apple\.com\/.{2}\/playlist\/.+$/;
 
 const is_valid_url = str => {
   const regexp = /^(?:(?:https?):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
@@ -134,7 +133,7 @@ function isAppleMusicPlaylistURL(url){
 }
 
 function isSupportedPlaylistURL(url){
-  return SUPPORTED_PLATFORMS_PLAYLIST_URL_REGEX.test(url)
+  return isSpotifyPlaylistURL(url) || isAppleMusicPlaylistURL(url)
 }
 
 
