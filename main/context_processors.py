bc888f6ed67a869363339c3f583ec2a250a916d0
Raymond Akornor <raymondakornor@gmail.com>
2022-03-19 19:32:16 +0000

    Make things consistent with other variables.

--- main/context_processors.py
+++ main/context_processors.py
@@ -5,4 +5,4 @@ from .utils import generate_auth_token, get_version
 def default_context(request):
     token = generate_auth_token()
     version = get_version()
-    return {"APPLE_DEVELOPER_TOKEN": token, "DEBUG": settings.DEBUG, "version": version}
+    return {"APPLE_DEVELOPER_TOKEN": token, "DEBUG": settings.DEBUG, "VERSION": version}
