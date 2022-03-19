c93cb58ee897f1c900fd6e88b8bdb9b421cf8dcd
Raymond Akornor <raymondakornor@gmail.com>
2022-03-19 16:32:14 +0000

    Use short git commit as playlistor version.

--- main/context_processors.py
+++ main/context_processors.py
@@ -1,8 +1,11 @@
+import subprocess
 from django.conf import settings
 from .utils import generate_auth_token
 
 
-
 def default_context(request):
     token = generate_auth_token()
-    return {"APPLE_DEVELOPER_TOKEN": token, "DEBUG": settings.DEBUG}
+    version = subprocess.check_output(
+        ["git", "rev-parse", "--short", "HEAD"], universal_newlines=True
+    ).strip()
+    return {"APPLE_DEVELOPER_TOKEN": token, "DEBUG": settings.DEBUG, "version": version}
