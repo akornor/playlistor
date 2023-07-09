df9c2acbf6a4e6da88b1460369fb2aa199f0fb89
Raymond Akornor <raymondakornor@gmail.com>
2023-07-09 15:20:25 +0000

    Add command to delete old playlists.
    We intend to run this with in a cron job to frequently remove old playlists from our library. This is necessary because spotify has an undocumented maximum number of playlists a user can create.
    
    `./manage.py delete_stale_playlists`

---
+++ main/management/commands/delete_stale_playlists.py
@@ -0,0 +1,17 @@
+from main.utils import get_spotify_client
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+	def handle(self, *args, **options):
+		sp = get_spotify_client()
+		# number of playlists -- we should make this configurable at some point in the future
+		N = 500
+		playlists = []
+		while N > 0:
+			playlists.extend(sp.current_user_playlists(offset=4000, limit=50)['items'])
+			N -= 50
+		for playlist in playlists:
+			sp.current_user_unfollow_playlist(playlist['id'])
+		self.stdout.write(self.style.SUCCESS('Successfully deleted playlists.'))
