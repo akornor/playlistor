a1be2925e54bbb35d217b6f6943449cbaf5559d7
Raymond Akornor <raymondakornor@gmail.com>
2023-11-19 14:23:03 +0000

    Update migration.

--- main/migrations/0003_move_playlists_from_redis_to_db.py
+++ main/migrations/0003_move_playlists_from_redis_to_db.py
@@ -8,16 +8,17 @@ from main.utils import get_redis_client
 
 
 def move_playlists_to_database(apps: StateApps, schema_editor: DatabaseSchemaEditor):
-    redis_client = get_redis_client()
-    Playlist = apps.get_model('main', 'Playlist')
-    n = redis_client.llen('playlists')
-    if n > 0:
-        playlists = redis_client.lrange('playlists', 0, -1)
-        playlists = [json.loads(playlist) for playlist in playlists[::-1]]
-        playlists = [Playlist(name=playlist['name'], spotify_url=playlist['spotify_url'], applemusic_url=playlist['applemusic_url']) for playlist in playlists]
-        Playlist.objects.bulk_create(playlists)
-        # redis_client.delete('playlists')
-        redis_client.incrby('counter:playlists', n)
+    # redis_client = get_redis_client()
+    # Playlist = apps.get_model('main', 'Playlist')
+    # n = redis_client.llen('playlists')
+    # if n > 0:
+    #     playlists = redis_client.lrange('playlists', 0, -1)
+    #     playlists = [json.loads(playlist) for playlist in playlists[::-1]]
+    #     playlists = [Playlist(name=playlist['name'], spotify_url=playlist['spotify_url'], applemusic_url=playlist['applemusic_url']) for playlist in playlists]
+    #     Playlist.objects.bulk_create(playlists)
+    #     # redis_client.delete('playlists')
+    #     redis_client.incrby('counter:playlists', n)
+    pass
     
 
 class Migration(migrations.Migration):
