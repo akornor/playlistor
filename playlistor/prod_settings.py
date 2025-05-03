233044bd211b6e6a9b06b3f4ff4e742f2259e0c1
Raymond Akornor <raymondakornor@gmail.com>
2025-05-03 12:32:39 +0000

    Don't send 404s to sentry.

--- playlistor/prod_settings.py
+++ playlistor/prod_settings.py
@@ -2,12 +2,26 @@ import sentry_sdk
 from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.redis import RedisIntegration
+from spotipy.exceptions import SpotifyException
 
 from .base_settings import *
+
+
+def before_send(event, hint):
+    if "exc_info" in hint:
+        exc_type, exc_value, tb = hint["exc_info"]
+
+        if isinstance(exc_value, SpotifyException):
+            if hasattr(exc_value, "http_status") and exc_value.http_status == 404:
+                return None
+
+    return event
+
 
 sentry_sdk.init(
     dsn=get_secret("SENTRY_DSN"),
     integrations=[DjangoIntegration(), CeleryIntegration(), RedisIntegration()],
+    before_send=before_send,
 )
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
